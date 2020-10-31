7a22d352a9902e50f76cbca9c908ca96b155810a
Jahmilli <sebastian.southern@gmail.com>
2020-10-31 13:48:43 +1100

    fixed up id

--- deploy/videos.sql
+++ deploy/videos.sql
@@ -3,10 +3,9 @@
 BEGIN;
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-
 CREATE TABLE videos (
-  video_id uuid DEFAULT uuid_generate_v4(),
-  user_id TEXT, -- Should be user uuid
+  id uuid DEFAULT uuid_generate_v4(),
+  user_id TEXT,
   name TEXT,
   description TEXT,
   is_processed BOOLEAN,
@@ -14,7 +13,8 @@ CREATE TABLE videos (
   type_of_shot TEXT,
   storage_uri TEXT,
   feedback TEXT,
-  uploaded_timestamp timestamptz -- Check is this is correct...
+  uploaded_timestamp timestamptz
 );
 
+INSERT INTO videos (user_id, name, description, is_processed, angle_of_shot, type_of_shot, storage_uri, feedback, uploaded_timestamp) VALUES ('test', 'test name', 'description test', 't', 'front facing', 'three pointer', 's3://dev-bucket/Threshholds.avi', '', current_timestamp);
 COMMIT;
